9d1b94a8b7e1b0bc241e05b6f6101bd5f970eec8
Sebastian Kaier <22768625+skaiwlkr@users.noreply.github.com>
2023-03-25 17:02:50 +0100

    added first iteration of api for links

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
+<script setup>
+    const { data: links } = await useFetch('/api/links')
+</script>
+
 <template>
     <div>
         <h1>Hello world!</h1>
+        <p>{{ links }}</p>
     </div>
 </template>
---
+++ server/api/links.ts
@@ -0,0 +1,14 @@
+export default defineEventHandler(() => (
+    [
+        {
+            name: 'Example 1',
+            url: 'https://google.com',
+            target: '_blank'
+        },
+        {
+            name: 'Example 2',
+            url: 'https://google.com',
+            target: '_blank'
+        }
+    ]
+));  
