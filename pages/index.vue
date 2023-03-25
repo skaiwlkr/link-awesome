c4ec37c1483599bbc0cf4fe308e34838634ff4b7
Sebastian Kaier <22768625+skaiwlkr@users.noreply.github.com>
2023-03-25 17:09:35 +0100

    added first iteration of api for bio

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <script setup>
+    const { data: bio } = await useFetch('/api/bio')
     const { data: links } = await useFetch('/api/links')
 </script>
 
 <template>
     <div>
         <h1>Hello world!</h1>
+        <p>{{ bio }}</p>
         <p>{{ links }}</p>
     </div>
 </template>
---
+++ server/api/bio.ts
@@ -0,0 +1,7 @@
+export default defineEventHandler(() => (
+    {
+        title: 'Hello world',
+        description: 'Your personal bio here',
+        subtext: 'Additional info'
+    }
+));  
