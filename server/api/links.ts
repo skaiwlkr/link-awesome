e4e49175adec9a233464cfa6fc778c55ee2edcb6
Sebastian Kaier <22768625+skaiwlkr@users.noreply.github.com>
2023-03-25 21:27:36 +0100

    updated links api

--- server/api/links.ts
+++ server/api/links.ts
@@ -1,14 +1,36 @@
 export default defineEventHandler(() => (
     [
         {
-            name: 'Example 1',
-            url: 'https://google.com',
-            target: '_blank'
+            group_title: 'Group 1',
+            links:
+            [
+                {
+                    name: 'Example 1',
+                    url: 'https://google.com',
+                    target: '_blank'
+                },
+                {
+                    name: 'Example 1',
+                    url: 'https://google.com',
+                    target: '_blank'
+                }
+            ]
         },
         {
-            name: 'Example 2',
-            url: 'https://google.com',
-            target: '_blank'
+            group_title: 'Group 2',
+            links:
+            [
+                {
+                    name: 'Example 1',
+                    url: 'https://google.com',
+                    target: '_blank'
+                },
+                {
+                    name: 'Example 1',
+                    url: 'https://google.com',
+                    target: '_blank'
+                }
+            ]
         }
     ]
 ));  
