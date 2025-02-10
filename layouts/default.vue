c607b0727e12331a08dc5c728a51b7c85c483a3e
Sebastian Kaier <22768625+skaiwlkr@users.noreply.github.com>
2025-02-10 12:31:36 +0100

    added general layout structure

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>
-        <slot />
-    </div>
+    <main class="min-h-screen">
+        <div class="min-h-screen flex flex-col justify-between items-center text-center p-6 m-auto md:w-128">
+            <div>
+                <div class="flex flex-col items-center mt-16 mb-3">
+                    <slot />
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
