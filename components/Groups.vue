b6d837ac5ccebf64bf899f5c06d1eef17070af38
Sebastian Kaier <22768625+skaiwlkr@users.noreply.github.com>
2025-02-14 22:56:19 +0100

    added components for groups and group links

---
+++ components/Groups.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="max-w-md w-full">
+        <div v-for="group in links" :key="group" class="flex flex-col mx-auto py-3">
+            <h3 class="uppercase tracking-widest text-slate-500 font-medium text-sm">{{ group.group }}</h3>
+            <div v-for="link in group.links">
+                <GroupLink :link="link" :key="link.title"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+.button-inner {
+    &:hover {
+      transition: all 333ms ease 0s;
+      border-color: white;
+    }
+
+    &::before {
+      content: "";
+      border-radius: 0.75rem;
+      display: block;
+      position: absolute;
+      z-index: -1;
+      inset: -2px;
+      opacity: 0;
+      transform: scale(0.0)
+    }
+
+    &:hover::before {
+      opacity: 1;
+      transition: all 333ms ease 0s;
+      transform: scale(1);
+    }
+  }
+</style>
+
+<script setup>
+const { data: links } = await useFetch('/api/links')
+
+defineProps ({
+    type: Object,
+    required: true
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,6 @@
     <div class="flex flex-col items-center mt-16 mb-3">
         <Avatar />
         <Bio />
-        <Links />
+        <Groups />
     </div>
 </template>
