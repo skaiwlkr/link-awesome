ae91001a99dc400b0573d73e3d7f48954edf60a6
Sebastian Kaier <22768625+skaiwlkr@users.noreply.github.com>
2025-02-15 11:12:21 +0100

    removed unnecessary styles

--- components/Groups.vue
+++ components/Groups.vue
@@ -9,32 +9,6 @@
     </div>
 </template>
 
-<style lang="scss">
-.button-inner {
-    &:hover {
-      transition: all 333ms ease 0s;
-      border-color: white;
-    }
-
-    &::before {
-      content: "";
-      border-radius: 0.75rem;
-      display: block;
-      position: absolute;
-      z-index: -1;
-      inset: -2px;
-      opacity: 0;
-      transform: scale(0.0)
-    }
-
-    &:hover::before {
-      opacity: 1;
-      transition: all 333ms ease 0s;
-      transform: scale(1);
-    }
-  }
-</style>
-
 <script setup>
 const { data: links } = await useFetch('/api/links')
 
