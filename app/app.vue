9a57fc87774ae3aa5242c6042c4ef74369328d28
Sebastian Kaier <22768625+skaiwlkr@users.noreply.github.com>
2025-02-20 20:23:17 +0100

    changed basic seo information

--- app/app.vue
+++ app/app.vue
@@ -3,3 +3,19 @@
         <NuxtPage />
     </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+useServerSeoMeta({
+  title: 'Link Awesome - Self-hosted Linktree alternative',
+  ogTitle: 'Link Awesome - Self-hosted Linktree alternative',
+  description: 'Self-host your own page for bio and links',
+  ogDescription: 'Self-host your own page for bio and links',
+  ogUrl: 'https://link-awesome.vercel.app/',
+  ogSiteName: 'Link Awesome - Self-hosted Linktree alternative',
+  ogImage: 'https://link-awesome.vercel.app/og-image.png',
+  twitterCard: 'summary_large_image',
+  twitterTitle: 'Link Awesome - Self-hosted Linktree alternative',
+  twitterImage: 'https://link-awesome.vercel.app/og-image.png',
+  twitterDescription: 'Link Awesome - Self-hosted Linktree alternative'
+})
+</script>
